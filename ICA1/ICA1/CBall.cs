f15284fb8c2312bf58be262e5b4ba7c363f8e8aa
coshust1 <122324549+coshust1@users.noreply.github.com>
2023-09-11 21:34:08 -0600

    ICA1

---
+++ ICA1/ICA1/CBall.cs
@@ -0,0 +1,65 @@
+﻿using GDIDrawer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA1
+{
+    public class CBall
+    {
+        // Declare private variables for the position, direction, color, and size of the ball.
+        private Point position;
+        private Point direction;
+        private Color color;
+        private int size;
+
+        // Constructor to create a new ball with the specified color and a default position, direction, and size.
+        public CBall(Color Color)
+        {
+            color = Color;
+            position = new Point(400, 300);
+            direction = new Point(100, 20);
+            size = 50;
+        }
+
+        // Constructor to create a new ball with the specified color and direction, and a default position and size.
+        public CBall(Color Color, Point Direction)
+        : this(Color)
+        {
+            direction = Direction;
+        }
+
+        // Method to move the ball.
+        public void Move()
+        {
+            // If the ball hits the left or right wall, reverse its x direction and decrease its size.
+            if (position.X < 0 || position.X > 800)
+            {
+                direction.X = -(direction.X);
+                size -= 2;
+            }
+            // If the ball hits the top or bottom wall, reverse its y direction and decrease its size.
+            if (position.Y < 0 || position.Y > 800)
+            {
+                direction.Y = -(direction.Y);
+                size -= 2;
+            }
+            // Update the position of the ball.
+            position.Offset(direction);
+        }
+
+        // Method to render the ball on the drawer.
+        public void Renderer(CDrawer drawer)
+        {
+            // Draw the ball on the drawer if its size is greater than 1.
+            if (size > 1)
+            {
+                drawer.AddCenteredEllipse(position, size, size, color);
+                drawer.Render();
+            }
+        }
+    }
+}
---
+++ ICA1/ICA1/Form1.cs
@@ -0,0 +1,75 @@
+﻿/////////////////////////////////////////////////////////
+///Caleb Oshust
+///Sept 11, 2023
+///CMPE 2300
+///ICA1 Balls
+///
+///Generate instances of ball class and move them in renderer
+///
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace ICA1
+{
+    public partial class Form1 : Form
+    {
+        // Declare drawer object for drawing, a random object for generating random numbers, and a list to store balls.
+        CDrawer drawer = new CDrawer(800, 800);
+        Random random = new Random();
+        List<CBall> list = new List<CBall>();
+
+        public Form1()
+        {
+            // Initialize the form components.
+            InitializeComponent();
+            // Disable continuous update of the drawer to improve performance.
+            drawer.ContinuousUpdate = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Declare variables to hold the x and y components of the direction.
+            int x = 0;
+            int y = 0;
+
+            // Generate random values for x and y until they are both non-zero and not equal.
+            while (x == 0 || y == 0 || x == y)
+            {
+                x = random.Next(-5, 6);
+                y = random.Next(-5, 6);
+            }
+
+            // Create a new CBall object with a random color and the generated direction, and add it to the list.
+            list.Add(new CBall(Color.FromArgb(random.Next(256), random.Next(256), random.Next(256)), new Point(x, y)));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create a new CBall object with a random color and the default direction, and add it to the list.
+            list.Add(new CBall(Color.FromArgb(random.Next(256), random.Next(256), random.Next(256))));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Clear the drawer.
+            drawer.Clear();
+            // Move and render each ball in the list.
+            foreach (var item in list)
+            {
+                item.Move();
+                item.Renderer(drawer);
+            }
+        }
+    }
+
+}
